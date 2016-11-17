efab6d58924ad13aed7f9abdb3e57c20bf24775d
Koh Tim Lin <timthree39@gmail.com>
2016-11-17 13:52:52 +0800

    New scenes added : Main menu   Town
    New classes added
    : Scene Navigation - use this class to go to different scenes (its using a scene manager)
    
    Classes modified
    : character - now with position and direction with movement
    : player - (temp) now moving a sprite to mouse position.

--- FYPJ2/Assets/Scripts/Player.cs
+++ FYPJ2/Assets/Scripts/Player.cs
@@ -12,12 +12,17 @@ public class Player : MonoBehaviour
         //get the "Character.cs" script from the player GameObject
         //Remember to add!
         p_base = GetComponent<Character>();
-
-        p_base.SetHealth(0);
     }
 
     // Update is called once per frame
     private void Update()
     {
+        //get the joystick movement and apply it here after adjusting the move speed (based on how far the drag from start moving)
+        MovePlayer((Input.mousePosition) );
+    }
+
+    public void MovePlayer(Vector2 _pos)
+    {
+        p_base.UpdatePosition(_pos, transform);
     }
 }
