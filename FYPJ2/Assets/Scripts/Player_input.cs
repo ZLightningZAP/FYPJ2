0e817bd4dbf08e665d040132e03ba052ffd45922
Koh Tim Lin <timthree39@gmail.com>
2016-12-26 17:15:49 +0800

    Linked the scenes from main menu added the typing mechanic into game assets added programmer art

---
+++ FYPJ2/Assets/Scripts/Player_input.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_input : MonoBehaviour
+{
+    public Text txt_required;
+    public Text txt_filled;
+    int char_index;
+    char[] char_required;
+
+    // Use this for initialization
+    void Start()
+    {
+        char_required = txt_required.text.ToCharArray();
+        char_index = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkKey();    
+    }
+
+    void checkKey()
+    {
+        for(var i = KeyCode.A ; i < KeyCode.Z; i++)
+        {
+            if (Input.GetKeyDown(i))
+            {
+                string crntChar = i.ToString();
+                if (crntChar == char_required[char_index].ToString().ToUpper())
+                {
+                    
+                    txt_filled.text += crntChar;
+                    char_index++;
+                }
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            if(txt_required.text == txt_filled.text)
+            {
+                //successful , change monster, drop gold..etc
+            }
+            else
+            {
+                txt_filled.text = "";
+                char_index = 0;
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.Backspace))
+        {
+            txt_filled.text = txt_filled.text.Remove(txt_filled.text.Length - 1);
+            char_index--;
+        }
+    }
+
+
+}
---
+++ FYPJ2/Assets/Scripts/Scene_navigation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_navigation : MonoBehaviour {
+
+	public void GoTo_Settings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void GoTo_Gameplay()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+    
+    public void GoTo_Exit()
+    {
+        Application.Quit();
+    }
+}
