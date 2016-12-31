42719608564f3bda2c687544ad013dd475c34aff
Keith <wongdaokang@gmail.com>
2016-12-31 16:54:41 +0800

    -  Fix linking between the main menu and settings -  Added some settings (WIP)

---
+++ FYPJ2/Assets/Scripts/Setting_DisplayNumber.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Setting_DisplayNumber : MonoBehaviour
+{
+    public Slider Slider;
+    private float Value;
+
+    // Use this for initialization
+    private void Start()
+    {
+        //Set slider to the max value
+        Slider.value = 100;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Value = Slider.value;
+        gameObject.GetComponent<Text>().text = Value.ToString();
+    }
+}
