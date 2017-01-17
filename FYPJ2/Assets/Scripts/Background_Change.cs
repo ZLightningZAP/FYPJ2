8e7152b790e08d96d5215dbbb988fbd98c33b5d7
Keith <wongdaokang@gmail.com>
2017-01-17 22:40:04 +0800

    - Implemented background change in the gameplay scene

---
+++ FYPJ2/Assets/Scripts/Background_Change.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Background_Change : MonoBehaviour
+{
+    public Sprite[] BackgroundArray;
+
+    private int i;
+
+    private void Start()
+    {
+        i = Random.Range(0, BackgroundArray.Length);
+        gameObject.GetComponent<Image>().sprite = BackgroundArray[i];
+    }
+
+    //Call this function to randomly change background
+    public void ChangeBG()
+    {
+        i = Random.Range(0, BackgroundArray.Length);
+        gameObject.GetComponent<Image>().sprite = BackgroundArray[i];
+    }
+}
