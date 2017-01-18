3ee6ba2b2f1c74239bb3956550a8a70dee02c2e9
KEITH\keith <wongdaokang@gmail.com>
2017-01-18 18:27:36 +0800

    - Added animation for boss

--- FYPJ2/Assets/Scripts/Boss_AI.cs
+++ FYPJ2/Assets/Scripts/Boss_AI.cs
@@ -3,61 +3,26 @@ using UnityEngine.UI;
 
 public class Boss_AI : MonoBehaviour
 {
-    public Image Boss_timer;
-    public Text TextFeedback; // use to display time left
-    public float f_totalTime;
-    public int i_totalHealth;
+    public GameObject[] Boss;
 
-    public float f_timeLeft;
-    private int i_healthLeft;
-
-    //Monster sprites
-    public Image monster_;
-
-    public Sprite[] spriteArr_boss;
-    public Sprite[] spriteArr_mob;
-
-    private int currentMonster;
+    private SpriteRenderer spriteRen;
+    private RuntimeAnimatorController animcontroller;
+    private int i = 0;
 
     // Use this for initialization
     private void Start()
     {
-        currentMonster = GameControl._control._monsterCount;
+        i = Random.Range(0, Boss.Length);
+        spriteRen = Boss[i].GetComponent<SpriteRenderer>();
+        animcontroller = Boss[i].GetComponent<Animator>().runtimeAnimatorController;
 
-        //Set time left to the total time
-        f_timeLeft = f_totalTime;
-        //Set boss health to the maximum health
-        i_healthLeft = i_totalHealth;
+        gameObject.GetComponent<SpriteRenderer>().sprite = spriteRen.sprite;
+        gameObject.GetComponent<Animator>().runtimeAnimatorController = animcontroller;
+        gameObject.GetComponent<Animator>().Stop();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if (GameControl._control._monsterCount < 10)
-        {
-            Boss_timer.fillAmount = 0;
-            TextFeedback.text = "";
-        }
-        else
-        {
-            f_timeLeft -= Time.deltaTime;
-
-            Boss_timer.fillAmount = f_timeLeft / f_totalTime;
-            TextFeedback.text = f_timeLeft.ToString();
-        }
-
-        if (currentMonster != GameControl._control._monsterCount)
-        {
-            if (GameControl._control._monsterCount < 10)
-            {
-                monster_.GetComponent<Image>().sprite = spriteArr_mob[0];
-            }
-            else
-            {
-                monster_.GetComponent<Image>().sprite = spriteArr_boss[0];
-            }
-
-            currentMonster = GameControl._control._monsterCount;
-        }
     }
 }
